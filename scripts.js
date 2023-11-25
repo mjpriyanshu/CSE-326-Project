f3505fa2e369fbc968e0c2e3639e276f36ab0404
Vaibhav Pratap Deo <148430757+vaibhavdeo21@users.noreply.github.com>
2023-11-26 00:53:43 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('bookingForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    window.location.href = "confirm.html"; 
+});
+document.getElementById('bookingForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Form submitted!');
+});
