25fa1dca23712a70ef84d1b6766f40bae63b03d6
Cipher <148776473+mjpriyanshu@users.noreply.github.com>
2023-11-28 18:57:25 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('bookingForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    
-    window.location.href = "confirm.html"; 
-});
-document.getElementById('bookingForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Form submitted!');
-});
